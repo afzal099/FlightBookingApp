61b0b87e6674d04fcc44c664bf147aa7e0b03f0a
Mohammed Afsul Mohammed Hussain <m.mohammedhussain@shell.com>
2018-10-21 13:10:10 +0530

    Home view UX finished

---
+++ FlightBookingApp/FlightBookingApp/HomePage/HomePageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomePageViewController.swift
+//  FlightBookingApp
+//
+//  Created by Mohammed Afsul Mohammed Hussain on 21/10/18.
+//  Copyright © 2018 RBT. All rights reserved.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Search Flight"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FlightBookingApp/Pods/Target Support Files/Pods-FlightBookingApp/Pods-FlightBookingApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlightBookingApp : NSObject
+@end
+@implementation PodsDummy_Pods_FlightBookingApp
+@end
